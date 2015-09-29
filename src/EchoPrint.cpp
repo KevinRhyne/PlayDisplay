a470c74e4b3c7cb74685534079f9235d3a1861d3
Kevin Rhyne <rhyne@gatech.edu>
2015-09-29 04:48:09 -0400

    lib structure changes

--- src/EchoPrint.cpp
+++ src/EchoPrint.cpp
@@ -4,3 +4,13 @@
 // EchoPrint codegen wrapper for SampleShipper
 //
 // Takes Audio object, extracts PCM, recieves Codegen response
+
+#include <stdio.h>
+#include "EchoPrint.h"
+#include "Codegen.h"
+
+int main(){
+	printf("Hello echoprint world\n");
+
+	 
+}
